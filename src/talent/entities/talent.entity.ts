7329d76ad0259b174194145a7c29e1f479aaecb0
rkota <rosairekota@gmail.com>
2023-01-30 23:27:09 +0100

    feat: add talent entity

--- src/talent/entities/talent.entity.ts
+++ src/talent/entities/talent.entity.ts
@@ -1,1 +1,39 @@
-export class Talent {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../auth/user/entities/user.entity';
+
+@Entity('talents')
+export class Talent {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  experience?: string;
+
+  @Column()
+  level?: string;
+
+  @Column()
+  education?: string;
+
+  @Column()
+  githubLink?: string;
+
+  @Column()
+  websiteLink?: string;
+
+  @Column()
+  location?: string;
+
+  @Column()
+  phone?: string;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+}
